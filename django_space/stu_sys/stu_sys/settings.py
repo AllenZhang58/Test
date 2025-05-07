61a6c471a5365d0f2e97dc77b6f86d7a1eeabdca
vboxuser <vboxuser@Rh-zh.myguest.virtualbox.org>
2025-05-07 12:01:23 -0400

    填充models.py文件

--- django_space/stu_sys/stu_sys/settings.py
+++ django_space/stu_sys/stu_sys/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #配置子应用
+    'stu_info.apps.SysInfoConfig'
 ]
 
 MIDDLEWARE = [
--- django_space/stu_sys/sys_info/models.py
+++ django_space/stu_sys/sys_info/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+'''
+1. 模型类需要继承自models.Model
+2. 系统自动为我们创建ID主键
+3. 字段定义：
+    字段名 = model.类型(选项)
+        字段名：数据表中的字段名；字段名不可以使用关键字（python，mysql等）
+'''
+
+#班级信息表
+class ClaInfo(models.Model):
+    name = models.CharField(max_length=10)
+    pass
+
+#年级信息表
+class GraInfo(models.Model):
+    name = models.CharField(max_length=10)
+    pass
+
+#学生信息表 必须要继承的对象是models.Model
+class StuInfo(models.Model):
+    name = models.CharField(max_length=10) # name 为字符串，长度为10
+    age = models.IntegerField()
+    gender = models.BooleanField()
+    is_clainfo = models.ForeignKey(ClaInfo, on_delete=models.CASCADE)
+    is_grainfo = models.ForeignKey(GraInfo, on_delete=models.CASCADE)
+    pass
